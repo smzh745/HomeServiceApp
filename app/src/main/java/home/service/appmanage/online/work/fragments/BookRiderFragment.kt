94b949ba99df2d5309bc32bc41e9a9fcef7820f0
thesmzh <thegame.mzh@gmail.com>
2021-01-03 20:41:03 +0500

    initial commit

--- app/src/main/java/home/service/appmanage/online/work/fragments/BookRiderFragment.kt
+++ app/src/main/java/home/service/appmanage/online/work/fragments/BookRiderFragment.kt
@@ -2,18 +2,34 @@ package home.service.appmanage.online.work.fragments
 
 import android.location.Location
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
 import home.service.appmanage.online.work.R
 import home.service.appmanage.online.work.utils.Constants.TAGI
+import java.util.ArrayList
 
 
-class BookRiderFragment : BaseFragment() {
+class BookRiderFragment : BaseFragment(), OnMapReadyCallback {
     private var dropUpLoc: String? = null
     private var picLoc: String? = null
     private var rideType: String? = null
+    private var options: PolylineOptions? = null
+    private lateinit var googleMap: GoogleMap
+    private var sourceLoc: Location? = null
+    private var descLoc: Location? = null
+    private var points: ArrayList<LatLng>? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,15 +40,25 @@ class BookRiderFragment : BaseFragment() {
         dropUpLoc = requireArguments().getString("dropOfflocation")
         picLoc = requireArguments().getString("droppiclocation")
 
-        val sourceLoc = Location("")
+        sourceLoc = Location("")
         val splitterSource = picLoc!!.split(",")
-        sourceLoc.latitude = splitterSource[0].toDouble()
-        sourceLoc.longitude = splitterSource[1].toDouble()
-        val descLoc = Location("")
+        sourceLoc!!.latitude = splitterSource[0].toDouble()
+        sourceLoc!!.longitude = splitterSource[1].toDouble()
+        descLoc = Location("")
         val splitterDesc = dropUpLoc!!.split(",")
-        descLoc.latitude = splitterDesc[0].toDouble()
-        descLoc.longitude = splitterDesc[1].toDouble()
-        Log.d(TAGI, "onCreateView: " + getTimeTaken(sourceLoc, descLoc))
+        descLoc!!.latitude = splitterDesc[0].toDouble()
+        descLoc!!.longitude = splitterDesc[1].toDouble()
+        Log.d(TAGI, "onCreateView: " + getTimeTaken(sourceLoc!!, descLoc!!))
+        showDialog("Loading Map..")
+        Handler().postDelayed({
+
+            val mapFragment = childFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment?
+            mapFragment!!.getMapAsync(this)
+
+        }, 2000)
+        options = PolylineOptions()
+        points = ArrayList()
 
         return root
     }
@@ -54,5 +80,57 @@ class BookRiderFragment : BaseFragment() {
         }
     }
 
-   
+    override fun onMapReady(p0: GoogleMap?) {
+        googleMap = p0!!
+        p0.mapType = GoogleMap.MAP_TYPE_NORMAL
+
+        p0.addMarker(
+            MarkerOptions()
+                .position(
+                    LatLng(
+                        sourceLoc!!.latitude,
+                        sourceLoc!!.longitude
+                    )
+                )
+                .title("PickUp Location")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        )
+        p0.addMarker(
+            MarkerOptions()
+                .position(
+                    LatLng(
+                        descLoc!!.latitude,
+                        descLoc!!.longitude
+                    )
+                )
+                .title("DropOff Location")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+        )
+        p0.moveCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                LatLng(
+                    sourceLoc!!.latitude,
+                    sourceLoc!!.longitude
+                ), 16.0f
+            )
+        )
+        val position = LatLng(
+            sourceLoc!!.latitude,
+            sourceLoc!!.longitude
+        )
+        val position1 = LatLng(
+            descLoc!!.latitude,
+            descLoc!!.longitude
+        )
+        points?.add(position)
+        points?.add(position1)
+        options?.addAll(points)
+        options!!.width(5F)
+        if (options != null) {
+            p0.addPolyline(options)
+        }
+        hideDialog()
+    }
+
+
 }
